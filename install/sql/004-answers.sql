4bdc1fc63658093148943ec6e098cf7c66f72547
pasglop <david.delpuech@gmail.com>
2023-07-05 17:42:56 +0200

    Trying to make cascading work.. but does not

--- install/sql/004-answers.sql
+++ install/sql/004-answers.sql
@@ -3,12 +3,14 @@ create table answers
     answer_id   integer
         constraint answers_pk
             primary key autoincrement,
-    question_id integer                             not null
-        constraint answers_questions_question_id_fk
-            references questions,
+    question_id integer                             not null,
     answer_text TEXT                                not null,
     is_correct  boolean   default false             not null,
     created_at  TIMESTAMP default CURRENT_TIMESTAMP not null,
-    updated_at  TIMESTAMP default CURRENT_TIMESTAMP not null
+    updated_at  TIMESTAMP default CURRENT_TIMESTAMP not null,
+  CONSTRAINT fk_questions
+    FOREIGN KEY (question_id)
+    REFERENCES questions(question_id)
+    ON DELETE CASCADE
 );
 
